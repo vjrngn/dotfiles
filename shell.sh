f483ca5205fabcaf63fbfb4496a111cabbcb6dcd
Vijay Rangan <vijay.rangan@gmail.com>
2015-05-14 15:45:51 +0530

    First commit

---
+++ shell.sh
@@ -0,0 +1,23 @@
+# Vagrant
+alias bootup="cd ~/code/Laravel\ Homestead && bash bootup.sh"
+alias vmssh="cd ~/code/Laravel\ Homestead/Homestead && vagrant ssh"
+alias shutDown="cd ~/code/Laravel\ Homestead/Homestead && vagrant halt"
+
+# Testing
+alias pspec="./vendor/bin/phpspec"
+alias punit="./vendor/bin/phpunit"
+
+
+alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+alias code="cd ~/code/"
+
+# Laravel CLI
+alias laravel="~/.composer/vendor/bin/laravel"
+
+# GIT
+alias gs="git status"
+alias gp="git push"
+alias gc="git commit"
+alias gaa="git add ."
+alias gb="git branch"
+alias gcout="git checkout"
